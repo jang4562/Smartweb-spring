4922ebe372dbeb379dbdbffccccdd42418f336bc
Unknown <hss0736@naver.com>
2019-01-22 16:46:07 +0900

    1월 22일 데이터베이스 테이블 조회 구현

--- src/main/java/kr/green/spring/service/BoardService.java
+++ src/main/java/kr/green/spring/service/BoardService.java
@@ -1,9 +1,12 @@
 package kr.green.spring.service;
 
+import java.util.List;
+
 import kr.green.spring.vo.BoardVo;
 
 public interface BoardService {
 
 	void register(BoardVo board);
 	
+	List<BoardVo> getBoards();
 }
--- src/main/java/kr/green/spring/service/BoardServiceImp.java
+++ src/main/java/kr/green/spring/service/BoardServiceImp.java
@@ -1,5 +1,7 @@
 package kr.green.spring.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,5 +19,8 @@ public class BoardServiceImp implements BoardService {
 		boardDao.setBoard(board);
 		
 	}
-	
+	@Override
+	public List<BoardVo> getBoards() {
+		return boardDao.getBoards();
+	}
 }
